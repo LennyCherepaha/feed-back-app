610db47f4fec4df474cfd79621323ff843c65639
Lenny Cherepaha <lenny.cherepaha@gmail.com>
2021-12-07 17:29:21 -0500

    Project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <Header>
+      <div className="container">
+        <h2>Feed Back UI</h2>
+      </div>
+    </Header>
+  );
+}
+
+export default Header;
